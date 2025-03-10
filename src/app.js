d93fafa7730ca2c304436ae2765f8f14b2e9a3f9
CeliaBillaud <celiabillaud2@gmail.com>
2025-03-10 17:08:33 +0100

    challenge done

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
+const userControllers = require("./controllers/userControllers")
+
+const port = process.env.APP_PORT;
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUsersById);
 
 module.exports = app;
